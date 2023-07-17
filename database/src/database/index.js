658fd3a8c3665d259b9cc933679f8ad13f8a49d0
AugustoCorbalan <auguscorbi@gmail.com>
2023-07-17 11:16:20 -0300

    config.env

--- database/src/database/index.js
+++ database/src/database/index.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const { MONGO_URI } = require("../config/envs");
 
-const conn = mongoose.createConnection("mongodb+srv://Admin:Admin@cluster0.pspouia.mongodb.net/star_wars");
+// const conn = mongoose.createConnection("mongodb+srv://Admin:Admin@cluster0.pspouia.mongodb.net/star_wars");
+const conn = mongoose.createConnection(MONGO_URI);
 
 module.exports = {
     Character : conn.model("characters", require("./schemas/carachterSchema")),
